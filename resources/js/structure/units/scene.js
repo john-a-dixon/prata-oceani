fe523ec9d0144390c8243e4063d2375bcf097793
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-18 09:10:11 -0700

    Deleted the 'Scene' object.

--- resources/js/structure/units/scene.js
+++ resources/js/structure/units/scene.js
@@ -1,23 +1,3 @@
-// THE SCENE OBJECT. THIS WILL LATER BE CONVERTED TO A SCENE CLASS
-const scene = (number, title = "Untitled") => {
-  return {
-    number,
-    title,
-    characters: {
-      // Denotes characters mentioned in the scene.
-      mentioned: [],
-      // Denotes characters actively participating in the scene.
-      active: []
-    },
-    conflict: {
-
-    },
-    world: { // All scene have world elements, such as a setting
-      setting: ""
-    }
-  }
-}
-
 class Scene {
   constructor() {
     this._number = 0
