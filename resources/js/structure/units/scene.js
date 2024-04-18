9ac34ed4aa250c47e5d88eaffb9f67ffd7212d74
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-18 09:09:54 -0700

    Created the 'Scene' class.

--- resources/js/structure/units/scene.js
+++ resources/js/structure/units/scene.js
@@ -20,6 +20,20 @@ const scene = (number, title = "Untitled") => {
 
 class Scene {
   constructor() {
+    this._number = 0
+    this._title = "Untitled"
+    this._characters = {
+      // Denotes characters actively participating in the scene.
+      mentioned: [],
+      // Denotes characters actively participating in the scene.
+      active: []
+    }
+    this._conflict = {
 
+    }
+    // All scene have world elements, such as a setting
+    this._world = {
+      setting: ""
+    }
   }
 };
