b7b7f6343e4be7093857d80610d8f58b013f2bf1
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-30 17:15:05 -0700

    Added an example of keys.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './css/App.css';
 
 function App() {
+  const abc = ["A", "B", "C"]
+  const abcList = abc.map((x, i) => <li key={i}>{x}</li>)
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello world!</h1>
+        <p>{4 + 5}</p>
+        <ul>{abcList}</ul>
       </header>
     </div>
   );
