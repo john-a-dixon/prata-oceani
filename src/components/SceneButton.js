ac91e417d7f26f0306115040a79c312bd427b0a9
John Andrew Dixon <john-a-dixon@outlook.com>
2024-05-01 17:18:58 -0700

    Demonstrated that the thing can work.

--- src/components/SceneButton.js
+++ src/components/SceneButton.js
@@ -1,7 +1,9 @@
 import "../css/style.css";
 
 function SceneButton() {
+  const scenes = [...Array(8)].map((x, index) => <li>Hello</li>);
 
+  return (<ul>{scenes}</ul>)
 };
 
 export default SceneButton;
